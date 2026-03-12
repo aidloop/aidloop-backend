14607e110ca79c1e6cdf7065fc7cc87e9aebbf3e
emmystrings911 <sectorloko12@gmail.com>
2026-03-12 23:20:04 +0100

    Add files via upload

---
+++ src/routes/application.routes.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+import {
+  registerForEvent,
+  cancelRegistration,
+  getMyRegistrations,
+  getEventRegistrations,
+  markAttendance
+} from "../controllers/application.controller.js";
+
+import { authorize } from "../middleware/rbac.middleware.js";
+
+const router = express.Router();
+
+
+router.post(
+  "/events/:eventId/register",
+  authorize("volunteer"),
+  registerForEvent
+);
+
+
+router.delete(
+  "/events/:eventId/register",
+  authorize("volunteer"),
+  cancelRegistration
+);
+
+
+router.get(
+  "/registrations/me",
+  authorize("volunteer"),
+  getMyRegistrations
+);
+
+
+router.get(
+  "/events/:eventId/registrations",
+  authorize("organizer", "admin"),
+  getEventRegistrations
+);
+
+
+router.patch(
+  "/registrations/:id/attendance",
+  authorize("organizer", "admin"),
+  markAttendance
+);
+
+export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 import express from "express"
 import userRoutes from "./user.routes.js"
 import authRoutes from "./auth.routes.js"
+import applicationRoutes from "./application.routes.js"
 import { authenticate } from "../middleware/auth.middleware.js"
 import { authLimiter } from "../controllers/auth.controller.js"
+import eventRoutes from "./event.routes.js"
+import certificateRoutes from "./certificate.routes.js"
+import adminRoutes from "./admin.routes.js"
+import ratingRoutes from "./rating.routes.js"
 
 
 
@@ -11,8 +16,10 @@ const router = express.Router();
 
 router.use("/user", authenticate, userRoutes);
 router.use("/auth", authLimiter, authRoutes);
-
-
-
+router.use("/application", authenticate, applicationRoutes)
+router.use("/events", eventRoutes)
+router.use("/certificates", certificateRoutes)
+router.use("/admin", adminRoutes)
+router.use("/", authenticate,ratingRoutes)
 
 export default router;
