34bbb7762b4813f384bf07185fddd12b3a438a04
emmystrings911 <sectorloko12@gmail.com>
2026-03-14 19:02:50 +0100

    Update certificate.controller.js

--- src/controllers/certificate.controller.js
+++ src/controllers/certificate.controller.js
@@ -1,15 +1,34 @@
-import { generateCertificate } from "../services/certificate.service.js";
-export const createCertificate = async (req, res) => {
-    try {
-        const { userId, eventId } = req.body
-        const certificate = await generateCertificate(userId, eventId)
-        res.status(201).json({
-            message: "Certificate generated",
-            certificate
-        })
-    } catch (error) {
-        res.status(400).json({
-            error: error.message
-        })
-    }
-}
+import Certificate from "../models/Certificate.js";
+
+export const verifyCertificate = async (req, res) => {
+
+  const certificate = await Certificate
+    .findById(req.params.id)
+    .populate("volunteerId")
+    .populate("eventId");
+
+  if (!certificate) {
+    return res.status(404).json({
+      message: "Certificate not found"
+    });
+  }
+
+  res.json({
+    volunteer: certificate.volunteerId.fullName,
+    event: certificate.eventId.name,
+    issuedAt: certificate.issuedAt
+  });
+};
+
+export const downloadCertificate = async (req, res) => {
+
+  const certificate = await Certificate.findById(req.params.id);
+
+  if (!certificate) {
+    return res.status(404).json({
+      message: "Certificate not found"
+    });
+  }
+
+  res.download(certificate.certificateUrl);
+};
