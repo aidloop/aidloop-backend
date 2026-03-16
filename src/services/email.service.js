ad316230f1c1d528773accc2f79c7302ea18fde2
emmystrings911 <sectorloko12@gmail.com>
2026-03-16 18:13:41 +0100

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -11,12 +11,13 @@ import OrganizerWelcome from "../emails/templates/organizerWelcome.js";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 10000
 });
 
 /* -------------------------------- */
