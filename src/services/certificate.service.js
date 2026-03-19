a1b3823246d131d9798a89062cb857f11a4ff904
emmystrings911 <sectorloko12@gmail.com>
2026-03-19 12:47:35 +0100

    Update certificate.service.js

--- src/services/certificate.service.js
+++ src/services/certificate.service.js
@@ -10,7 +10,7 @@ export const generateCertificateService = async (registrationId) => {
     .populate({
       path: "eventId",
       populate: {
-        path: "organizationId" // ✅ this is your organizer
+        path: "organizationId"
       }
     });
 
@@ -18,34 +18,29 @@ export const generateCertificateService = async (registrationId) => {
     throw new Error("Registration not found");
   }
 
-  /* ✅ DEFINE ORGANIZER PROPERLY */
   const organizer = registration.eventId.organizationId;
   console.log(organizer);
   if (!organizer) {
     throw new Error("Organizer not found");
   }
 
-  /* ✅ CREATE CERTIFICATE */
+  /* CREATE CERTIFICATE */
   const certificate = await Certificate.create({
     volunteerId: registration.volunteerId._id,
     eventId: registration.eventId._id,
     registrationId
   });
 
-  /* ✅ GENERATE CERTIFICATE */
+  /*GENERATE CERTIFICATE */
   const url = await generateCertificate(
     registration.volunteerId.fullName,
     registration.eventId.name,
     organizer.fullName,
-    organizer.profileImage, // ✅ THIS is the logo
+    organizer.profileImage, //THIS is the logo
     organizer.signatureImage,
     certificate._id
   );
 
-  /* ✅ DEBUG LOGS */
-  console.log("Organizer:", organizer);
-  console.log("Organizer Logo:", organizer.profileImage);
-  console.log("Organizer Signature:", organizer.signatureImage);
   certificate.certificateUrl = url;
 
   await certificate.save();
