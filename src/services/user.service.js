491209657e46d62c5986295509b4aa24cdf03b72
emmystrings911 <sectorloko12@gmail.com>
2026-03-19 13:08:22 +0100

    added signature image user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,7 +9,7 @@ export const updateUserProfileService = async (userId, updateData) => {
   const allowedFields = [
     "fullName",
     "phoneNumber",
-    "signatureImage"
+    "signatureImage",
     "profileImage",
     "skills",
     "interests",
