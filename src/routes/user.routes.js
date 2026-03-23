cb7d66b620d55bd9c8b5c8c86980fc5cb035ae85
emmystrings911 <sectorloko12@gmail.com>
2026-03-23 18:07:34 +0100

    Add files via upload

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,9 @@ import {
     getMyProfile,
     updateMyProfile,
     getAllUsers,
-    changeUserRole
+    changeUserRole, uploadProfileImage
 } from "../controllers/user.controller.js";
+import upload from "../middleware/upload.middleware.js";
 import { authorize } from "../middleware/rbac.middleware.js";
 
 const router = express.Router();
@@ -13,5 +14,10 @@ router.get("/me", getMyProfile);
 router.put("/me", updateMyProfile);
 router.get("/", authorize("admin"),getAllUsers);
 router.patch("/:id/role", authorize("admin"),changeUserRole);
+router.post(
+  "/profile-image",
+  upload.single("image"),
+  uploadProfileImage
+);
 
 export default router;
