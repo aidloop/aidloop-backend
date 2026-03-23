73e78858b8324b8cf32823b85f70282328a97c28
emmystrings911 <sectorloko12@gmail.com>
2026-03-23 16:36:47 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,26 +7,31 @@ import { errorHandler } from "./middleware/error.middleware.js";
 
 const PORT = process.env.PORT || 3000;
 
+// Security
 app.use(helmet());
+
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
+      "https://aidloop-frontend.onrender.com" 
+    ],
+    credentials: true
   })
 );
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("mongo connected");
-
-    app.listen(PORT, () => {
-      console.log(`server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
-
 app.use(errorHandler);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("mongo connected");
+
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
