85c426d156e6b418f60dc90d36094494db732c04
emmystrings911 <sectorloko12@gmail.com>
2026-03-23 16:49:43 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,35 +1,8 @@
 import "./config/env.js";
 import app from "./app.js";
 import mongoose from "mongoose";
-import helmet from "helmet";
-import cors from "cors";
-import { errorHandler } from "./middleware/error.middleware.js";
 
 const PORT = process.env.PORT || 3000;
-
-// Security
-app.use(helmet());
-
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
-
-app.use(errorHandler);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
@@ -38,6 +11,7 @@ mongoose.connect(process.env.MONGO_URI)
   app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
   });
+
 })
 .catch((err) => {
   console.error("MongoDB connection error:", err);
