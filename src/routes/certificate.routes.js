0ccce70ae60d285b487c5e811eceadccd1a3e432
emmystrings911 <sectorloko12@gmail.com>
2026-03-26 01:43:53 +0100

    added new routes certificate.routes.js

--- src/routes/certificate.routes.js
+++ src/routes/certificate.routes.js
@@ -1,16 +1,25 @@
 import express from "express";
-import { verifyCertificate, downloadCertificate,
-    getMyCertificates,getCertificateById, getAllCertificates,getPendingCertificates
- } from "../controllers/certificate.controller.js";
- 
+import {
+  verifyCertificate,
+  downloadCertificate,
+  getMyCertificates,
+  getCertificateById,
+  getAllCertificates,
+  getPendingCertificates,
+  getCertificatesByEvent   
+} from "../controllers/certificate.controller.js";
 
 const router = express.Router();
 
 router.get("/verify/:id", verifyCertificate);
-router.get("/download/:id", downloadCertificate)
+router.get("/download/:id", downloadCertificate);
 router.get("/my-certificates", getMyCertificates);
+
+router.get("/event/:eventId", getCertificatesByEvent); 
+
+router.get("/pending/:eventId", getPendingCertificates);
+
 router.get("/:id", getCertificateById);
 router.get("/", getAllCertificates);
-router.get("/pending/:eventId", getPendingCertificates);
 
 export default router;
