eca359ed27695a6d40c1763511241cc5d329df46
emmystrings911 <sectorloko12@gmail.com>
2026-06-09 20:44:37 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "https://aidloop-frontendweb-self.vercel.app",
-       "https://aidloop.vercel.app",
+       "https://aidloop-fedweb.vercel.app",
        "https://aidloop.netlify.app",
       "https://aidloopfweb.vercel.app"
     ],
